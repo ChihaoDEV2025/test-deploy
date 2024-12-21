c00206539ab7bbee25391311ff67604804251f57
Chi Hao <chihao.dev@gmai.com>
2024-12-21 16:10:30 +0700

    first commit

---
+++ OneHundredButton/main.js
@@ -0,0 +1,27 @@
+//tim cha
+const parentNode = document.getElementById("container");
+
+//Tao con
+for (let i = 0; i < 100; i++) {
+  const button = document.createElement("button");
+  button.setAttribute("class", `button-${i}`);
+  button.textContent = `Button ${i}`;
+
+  button.style.fontSize = "20px";
+
+  if (i % 2 === 0) {
+    button.style.background = "red";
+  }
+
+  let click = parentNode.appendChild(button);
+
+  if (i % 2 === 0) {
+    click.addEventListener("click", function () {
+      alert(`"Fuck you! Bitch " - Hào said`);
+    });
+  } else {
+    click.addEventListener("click", function () {
+      alert(`"Mẹ Mày Béo"- Hào said!`);
+    });
+  }
+}
